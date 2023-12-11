bc258a80c0aeeabf7103a14f4eb6173f536289d8
Ipek Efendiev <ipek.efendiev@politik.ch>
2023-12-11 23:35:29 +0300

    add ninja svg and use it in ninja cards

--- client/src/components/NinjaCard.tsx
+++ client/src/components/NinjaCard.tsx
@@ -1,16 +1,11 @@
-import { BeltOptions, NinjaProps } from '@/types';
-
-const colorClasses = {
-  [BeltOptions.BLACK]: 'bg-black',
-  [BeltOptions.ORANGE]: 'bg-orange-400',
-  [BeltOptions.WHITE]: 'bg-white',
-};
+import { NinjaProps } from '@/types';
+import { NinjaIcon } from './NinjaIcon';
 
 export const NinjaCard = ({ ninja }: { ninja: NinjaProps }) => (
   <li className="text-left w-full">
     <div className="w-full flex gap-2 items-center">
+      <NinjaIcon color={ninja.belt} />
       <div className="text-white text-xl ">{ninja.name}</div>
-      <div className={`w-4 h-4 ${colorClasses[ninja.belt]}`} />
     </div>
   </li>
 );
